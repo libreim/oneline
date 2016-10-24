3bccf70f1ad161e0238abc89d729a727b73cc62c
David Charte <fdavidcl@protonmail.com>
2016-10-25 00:48:22 +0200

    Minor changes

--- oneline.rb
+++ oneline.rb
@@ -15,7 +15,7 @@ inner_product = v1.zip(v2).inject(0) { |p, (n, m)| p + n * m }
 # Start a web server in the current directory
 WEBrick::HTTPServer.new(:Port => 3000, :DocumentRoot => Dir.pwd).start
 
-# The first descent search algorithm
-#   This was actually implemented in https://github.com/fdavidcl/feature-selection/blob/1dc20e18303494ed58766a4f017005295e77c321/lib/feature-selection/heuristics/first_descent.rb
+# Search the next solution of the First Ascent/Descent hill-climbing algorithm
+#   This was actually implemented in https://git.io/vPxQ6
 #   but changed in later versions
 solution.neighborhood.detect { |attempt, fitness| fitness > @current_fitness }
