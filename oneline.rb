eb09449058c539d99c04db2fb72a0c71890f2ada
David Charte <fdavidcl@protonmail.com>
2016-11-17 02:20:58 +0100

    Some style changes

--- oneline.rb
+++ oneline.rb
@@ -1,5 +1,6 @@
 #!/bin/ruby
 require "colorize"
+#----------------------------------------------------------------------------------------------------------
 # This is a hacky way of just executing the last line of the file in Ruby
 eval File.readlines(__FILE__).last
 exit
@@ -16,9 +17,12 @@ exit
 puts "Hello World!"
 
 ################################################################################
-# 2. Swap two objects                                                          #
+# 2. Do multiple assignments                                                   #
 ################################################################################
+one, two, three = 1, 2, 3
+# In particular, swap two variables and some Haskell-style pattern matching    #
 one, two = two, one
+x, *xs = [1, 2, 3, 4]
 
 ################################################################################
 # 3. Cheer up on Fridays                                                       #
@@ -37,7 +41,7 @@ puts list.each_with_index.map { |w,i| "#{i}. #{w}" }
 v1 = [6, 4, -1]
 v2 = [-9, 0, 1]
 inner_product = v1.zip(v2).inject(0) { |p, (n, m)| p + n * m }
-# result: -55
+# result: -55                                                                  #
 
 ################################################################################
 # BONUS! Check primality                                                       #
@@ -48,7 +52,7 @@ inner_product = v1.zip(v2).inject(0) { |p, (n, m)| p + n * m }
 # 6. Create your own type of exception                                         #
 ################################################################################
 class MyCustomError < StandardError; end
-# Later you can `raise MyCustomError`
+# Later you can `raise MyCustomError`                                          #
 
 ################################################################################
 # 7. Start a web server in the current directory                               #
@@ -66,9 +70,9 @@ solution.neighborhood.detect { |attempt, fitness| fitness > @current_fitness }
 # 9. Generate a Hash that calculates the Fibonacci sequence                    #
 ################################################################################
 fibonacci = Hash.new { |h, i| h[i] = h[i - 2] + h[i - 1] }.update(0 => 0, 1 => 1)
-# result:
-# > fibonacci[200]
-#=> 280571172992510140037611932413038677189525
+# result:                                                                      #
+# > fibonacci[200]                                                             #
+#=> 280571172992510140037611932413038677189525                                 #
 
 ################################################################################
 # 10. Display a file by blocks, as a presentation                              #
